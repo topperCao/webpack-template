0cc8bfbd12d48f18deb91c935c4e37567414122a
caozf@fun.tv <caozf@fun.tv>
2017-09-11 17:57:49 +0800

    {gulp server}

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 /**
- * Created by sloong on 2016/6/14.
+ * Modifyed by caozf on 2017/9/11.
  */
 'use strict';
 
 var gulp = require('gulp');
 var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var webpack_config = require('./webpack.config');
 
 //用于gulp传递参数
 var minimist = require('minimist');
@@ -16,13 +18,15 @@ var assets = process.cwd() + '/dist';
 
 var knownOptions = {
     string: 'env',
-    default: {env: process.env.NODE_ENV || 'production'}
+    default: {env: process.env.NODE_ENV || 'debug'}
 };
 
 var options = minimist(process.argv.slice(2), knownOptions);
 
-var webpackConf = require('./webpack.config');
-var webpackConfDev = require('./webpack-dev.config');
+// var webpackConf = require('./webpack.config');
+// var webpackConfDev = require('./webpack-dev.config');
+
+var webpackConfig = options.env === 'production' ? webpack_config({debug:false}) : webpack_config({debug:true});
 
 var remoteServer = {
     host: '192.168.56.129',
@@ -58,11 +62,26 @@ gulp.task('clean', ['hint'], function () {
 
 //run webpack pack
 gulp.task('pack', ['clean'], function (done) {
-    var _conf = options.env === 'production' ? webpackConf : webpackConfDev;
-    webpack(_conf, function (err, stats) {
+    var config = webpackConfig;
+    webpack(config, function (err, stats) {
         if (err) throw new gutil.PluginError('webpack', err)
         gutil.log('[webpack]', stats.toString({colors: true}))
         done()
+    });
+});
+//run webpack-dev-server
+gulp.task('server', function () {
+    var config = webpackConfig;
+    config.entry.vendor.unshift('webpack-dev-server/client?http://localhost:8080/', 'webpack/hot/dev-server');
+    new WebpackDevServer(webpack(config), {
+        contentBase:'./dist',
+        publicPath: config.output.publicPath,
+        hot: true
+    }).listen(8080, 'localhost', function(err, result) {
+        if (err) {
+            console.log(err);
+        }
+        console.log('Listening at localhost:8080');
     });
 });
 
--- webpack-dev.config.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author:sloong
- * @Date: 2016-06-14
- */
-'use strict';
-
-var webpack_config = require('./webpack.config.allinone')
-
-module.exports = webpack_config({debug:true})
